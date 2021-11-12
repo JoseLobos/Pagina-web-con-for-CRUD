91ec61ffe54154e1f247bbf0c08ffb464bb8fd2c
José Alexander Salinas Lobos <alexlobos303@gmail.com>
2021-11-12 17:04:49 -0600

    creacion de login

---
+++ Pagina web sistema de ventas/Login.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Login" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Pagina_web_sistema_de_ventas.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+    </p>
+    <p>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ingresar" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ Pagina web sistema de ventas/Login.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Pagina_web_sistema_de_ventas
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=LAPTOP-QM112JVD\MSSQLSERVER01;Initial Catalog=Login;Integrated Security=True");
+            SqlCommand comando = new SqlCommand("  Select count(*) From Usuarios1 where Id_usuario=@Usuarios  ", conexion);
+            conexion.Open();
+
+
+
+            if (TextBox1.Text == "" & TextBox2.Text == "")
+            {
+                MessageBox.Show("Hay campos vacios");
+
+
+
+            }
+            else
+            {
+                comando.Parameters.AddWithValue("@Usuarios", Convert.ToInt32(TextBox1.Text));
+                int i = Convert.ToInt32(comando.ExecuteScalar());
+                if (i > 0)
+                {
+                    try
+                    {
+
+
+
+                        MessageBox.Show("Sesion Iniciada");
+                        Response.Redirect("~/Default");
+                    }
+
+
+
+                    catch (SqlException k)
+                    {
+                        MessageBox.Show(k.ToString());
+                    }
+
+                   
+                }
+            }
+    }
+}
+}
